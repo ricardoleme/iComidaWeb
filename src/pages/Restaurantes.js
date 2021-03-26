5a8001f79de3daecf479eacf97b438bee8ced515
Ricardo Leme <ricardo.leme@gmail.com>
2021-03-26 15:51:18 -0300

    Ajustes no Restaurante

--- src/pages/Restaurantes.js
+++ src/pages/Restaurantes.js
@@ -1,39 +1,64 @@
 import { useRef, useState } from 'react';
 import { BACKEND } from '../constants'
+import ProgressBar from 'react-bootstrap/ProgressBar'
+import Button from 'react-bootstrap/Button'
+import Image from 'react-bootstrap/Image'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
 
-//https://github.com/JakeHartnell/react-images-upload
 
 function Restaurantes() {
-  const filesElement = useRef(null);
-  const [imagem, setImagem] = useState({
+  const filesElement = useRef(null)
+  const imagemVazia = {
     originalname: '',
     path: '',
     size: 0,
     mimetype: ''
-  })
+  }
+  const [progressoImagem, setProgressoImagem] = useState(0)
+  const [imagem, setImagem] = useState(imagemVazia)
 
-  const sendFile = async () => {
+
+
+
+  const enviaArquivo = async (event) => {
+    setProgressoImagem(10)
     const dataForm = new FormData();
     for (const file of filesElement.current.files) {
       dataForm.append('file', file);
     }
+    setProgressoImagem(30)
     const res = await fetch(`http://localhost:4000/upload`, {
       method: 'POST',
       body: dataForm,
     });
     const data = await res.json();
+    setProgressoImagem(100)
+
     const { originalname, path, size, mimetype } = data.files[0]
     setImagem({ originalname: originalname, path: path, mimetype: mimetype, size: size })
+    setProgressoImagem(0)
+    
   };
 
+
   return (
-    <div>
-      <input type="file" ref={filesElement} />
-      <button onClick={sendFile}>Send file</button>
-      {imagem &&
-        <img src={`${BACKEND}/${imagem.path}`} title={imagem.originalname} alt="oi" />
-      }
-    </div>
+    <Row>
+      <Col>
+        {progressoImagem > 0 && <ProgressBar animated now={progressoImagem} label={`${progressoImagem}%`} />}
+        <Form>
+          <Form.Group controlId="imagem">
+            <Form.Label>Foto do Restaurante:</Form.Label>
+            {imagem.size > 0 &&
+              <Image src={`${BACKEND}/${imagem.path}`} thumbnail width="100" title={imagem.originalname} alt={imagem.originalname} />
+            }
+            <Form.Control type="file" ref={filesElement} />
+          </Form.Group>
+          <Button variant="success" onClick={enviaArquivo}>Enviar Arquivo</Button>
+        </Form>
+      </Col>
+    </Row>
   );
 }
 
